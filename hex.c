e2946a66c36cfce0ff40d31261c534ec0d067948
krilhoho <kirillkhokhlov641@gmail.com>
2022-12-18 21:56:47 +0300

    modified hex/oct.c because of test fails

--- hex.c
+++ hex.c
@@ -3,39 +3,45 @@
 #include <string.h>
 
 int fhex(char *num) {
-    int res = 0;
-    for (int i = 2; i < strlen(num); i++) {
-        if (num[i] >= '0' && num[i] <= '9') {
-            res = res * 16 + (num[i] - '0');
-        } else if (num[i] >= 'a' && num[i] <= 'f') {
-            res = res * 16 + (num[i] - 'a' + 10);
-        } else if (num[i] >= 'A' && num[i] <= 'F') {
-            res = res * 16 + (num[i] - 'A' + 10);
-        }
-    }
-    return res;
+	int res = 0;
+	int sign = 1;
+	int i = 2;
+	if (number[0] == '-'){
+		sign = -1;
+		i = 3;
+	}
+    	for (; i < strlen(num); i++) {
+        	if (num[i] >= '0' && num[i] <= '9') {
+        		res = res * 16 + (num[i] - '0');
+        	} else if (num[i] >= 'a' && num[i] <= 'f') {
+            		res = res * 16 + (num[i] - 'a' + 10);
+        	} else if (num[i] >= 'A' && num[i] <= 'F') {
+            		res = res * 16 + (num[i] - 'A' + 10);
+        	}
+    	}
+    	return sign*res;
 }
 
 
 char *thex(int num) {
-    char *res = (char *) malloc(sizeof(char));
-    while (num > 0) {
-        int digit = num % 16;
-        if (digit >= 0 && digit <= 9) {
-            res = (char *) realloc(res, (strlen(res) + 1) * sizeof(char));
-            res[strlen(res)] = digit + '0';
-        } else {
-            res = (char *) realloc(res, (strlen(res) + 1) * sizeof(char));
-            res[strlen(res)] = digit - 10 + 'A';
-        }
-        num /= 16;
-    }
+	char *res = (char *) malloc(sizeof(char));
+	while (num > 0) {
+		int digit = num % 16;
+		if (digit >= 0 && digit <= 9) {
+			res = (char *) realloc(res, (strlen(res) + 1) * sizeof(char));
+            		res[strlen(res)] = digit + '0';
+        	} else {
+            		res = (char *) realloc(res, (strlen(res) + 1) * sizeof(char));
+            		res[strlen(res)] = digit - 10 + 'A';
+        	}
+        	num /= 16;
+    	}
 
-    char *res_rev = (char *) malloc(sizeof(char));
-    for (int i = strlen(res) - 1; i >= 0; i--) {
-        res_rev = (char *) realloc(res_rev, (strlen(res_rev) + 1) * sizeof(char));
-        res_rev[strlen(res_rev)] = res[i];
-    }
-    free(res);
-    return res_rev;
+    	char *res_rev = (char *) malloc(sizeof(char));
+    	for (int i = strlen(res) - 1; i >= 0; i--) {
+        	res_rev = (char *) realloc(res_rev, (strlen(res_rev) + 1) * sizeof(char));
+        	res_rev[strlen(res_rev)] = res[i];
+    	}
+    	free(res);
+    	return res_rev;
 }
--- oct.c
+++ oct.c
@@ -3,29 +3,35 @@
 #include <stdlib.h>
 
 char* toct(int num) {
-    char* res = (char*)malloc(sizeof(char));
-    while (num > 0) {
-        int digit = num % 8;
-        res = (char*)realloc(res, (strlen(res) + 1) * sizeof(char));
-        res[strlen(res)] = digit + '0';
-        num /= 8;
-    }
+   	char* res = (char*)malloc(sizeof(char));
+   	while (num > 0) {
+       		int digit = num % 8;
+       		res = (char*)realloc(res, (strlen(res) + 1) * sizeof(char));
+       		res[strlen(res)] = digit + '0';
+       		num /= 8;
+   	}
 
-    char* res_rev = (char*)malloc(sizeof(char));
-    for (int i = strlen(res) - 1; i >= 0; i--) {
-        res_rev = (char*)realloc(res_rev, (strlen(res_rev) + 1) * sizeof(char));
-        res_rev[strlen(res_rev)] = res[i];
-    }
-    free(res);
-    return res_rev;
+   	char* res_rev = (char*)malloc(sizeof(char));
+   	for (int i = strlen(res) - 1; i >= 0; i--) {
+       		res_rev = (char*)realloc(res_rev, (strlen(res_rev) + 1) * sizeof(char));
+       		res_rev[strlen(res_rev)] = res[i];
+   	}
+   	free(res);
+   	return res_rev;
 }
 
 int foct(char *num) {
-    int res = 0;
-    for (int i = 1; i < strlen(num); i++) {
-        if (num[i] >= '0' && num[i] <= '7') {
-            res = res * 8 + (num[i] - '0');
-        }
-    }
-    return res;
+   	int res = 0;
+	int sign =1;
+	int i =1;
+	if (num[0] == '-'){
+		sign = -1;
+		i =2;
+	}
+    	for (int i = 1; i < strlen(num); i++) {
+        	if (num[i] >= '0' && num[i] <= '7') {
+           	res = res * 8 + (num[i] - '0');
+       		}
+   	}
+   	return res;
 }
