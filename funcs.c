36cfd874cb0d33d2b1f4ec0acdf59414cfabc35d
krilhoho <kirillkhokhlov641@gmail.com>
2022-12-18 21:15:25 +0300

    funcs.c modified because of test fails

--- funcs.c
+++ funcs.c
@@ -3,116 +3,168 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+
 #include "hex.h"
 #include "oct.h"
 #include "bin.h"
 
-int operation(int a, int b, char op){
-	switch(op){
-		case '+':
-			return a+b;
-		case '-':
-			return a-b;
-		case '*':
-			return a*b;
-		case '/':
-			return a/b;
-		case '&':
-			return a&b;
-		case '|':
-			return a|b;
-		case '^':
-			return a^b;
-		default:
-			return -1;
-	}
+
+int operation(int a, int b, char op) {
+    switch (op) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            return a / b;
+        case '&':
+            return a & b;
+        case '|':
+            return a | b;
+        case '^':
+            return a ^ b;
+        default:
+            return -1;
+    }
 }
 
-void print(int n, int fl){
-	switch (fl){
-		case 2:
-			printf("%s (%d)\n", tbin(n), n);
-			break;
-		case 1:
-			printf("%s (%d)\n", thex(n), n);
-			break;
-		case 0:
-			printf("%s (%d)\n", toct(n), n);
-			break;
-		default:
-			return;
-	}
+void print(int n, int fl) {
+    char *res;
+    int len;
+    int sign = 1;
+    if (n < 0) {
+        sign = -1;
+        printf("-");
+    }
+
+    switch (fl) {
+        case 0:
+            res = to_hex(n * sign);
+            len = strlen(res);
+            if (len) {
+                printf("0x%s (%d)\n", res, n);
+            } else {
+                printf("0x0 0\n");
+            }
+            break;
+        case 1:
+            res = to_oct(n * sign);
+            len = strlen(res);
+            if (len) {
+                printf("0%s (%d)\n", res, n);
+            } else {
+                printf("00 0\n");
+            }
+            break;
+        case 2:
+            res = to_bin(n * sign);
+            len = strlen(res);
+            if (len) {
+                printf("%s (%d)\n", res, n);
+            } else {
+                printf("0 0\n");
+            }
+            break;
+        default:
+            return;
+    }
+    free(res);
 }
 
-int calc(char *str){
-	char *n1 = (char *) malloc(sizeof(char));
-	char *n2 = (char *) malloc(sizeof(char));
-	char op[1];
-	int fl = 0;
-	int n1_int;
-	int n2_int;
-	int res;
-
-	for (int i = 0; i<strlen(str); i++){
-		if (isspace(str[i])) {
-			continue;
-		}
-		if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/'){
-			op[0] = str[i];
-			fl = 1;
-			continue;
-		}
-		if (fl == 0){
-			n1 = (char *) realloc(n1, (strlen(n1)+1)*sizeof(char));
-			n1[strlen(n1)] = str[i];
-		} else {
-			n2 = (char *) realloc(n2, (strlen(n2)+1)*sizeof(char));
-			n2[strlen(n2)] = str[i];
-		}
-	}
-	
-	if (n1[0] == '0' && n2[0] == '0'){
-		fl = 0;
-		n1_int = foct(n1);
-		n2_int = foct(n2);
-	} else if (n1[0] == '0' && n1[1] == 'x' && n2[0] == '0' && n2[1] == 'x'){
-		fl = 1;
-		n1_int = fhex(n1);
-		n2_int = fhex(n2);
-	} else if (n1[0] == '1' && n2[0] == '1'){
-		fl = 2;
-		n1_int = fbin(n1);
-		n2_int = fbin(n2);
-	} else {
-		return 1;
-	}
-	free(n1);
-	free(n2);
-	res = operation(n1_int, n2_int, op[0]);
-	print(res, fl);
-	return 0;
+int check_system(char *str) {
+    int i = 0;
+    if (str[0] == '-') {
+        i = 1;
+    }
+    if (str[i] == '0' && str[i + 1] == 'x') {
+        return 0;
+    } else if (str[i] == '0') {
+        return 1;
+    } else {
+        return 2;
+    }
 }
 
+int convert(char *str, int fl) {
+    switch (fl) {
+        case 0:
+            return from_hex(str);
+        case 1:
+            return from_oct(str);
+        case 2:
+            return from_bin(str);
+        default:
+            return -1;
+    }
+}
 
+int for_one_number(char *str) {
+    char number[128];
+    memset(number, 0, sizeof(number));
 
+    for (int i = 1; i < strlen(str); i++) {
+        if (isspace(str[i])) {
+            continue;
+        }
+        number[strlen(number)] = str[i];
+    }
 
+    int flag = check_system(str);
+    int num = convert(number, flag);
 
+    print(~num, flag);
 
+    return 0;
+}
 
+int for_two_numbers(char *str) {
+    char num1[128];
+    char num2[128];
+    memset(num1, 0, sizeof(num1));
+    memset(num2, 0, sizeof(num2));
 
+    char op;
+    int fl = 0;
 
+    for (int i = 0; i < strlen(str); i++) {
+        if (isspace(str[i])) {
+            continue;
+        }
+        if ((str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/' || str[i] == '&' || str[i] == '|' ||
+             str[i] == '^') && fl == 1) {
+            op = str[i];
+            fl = 2;
+            continue;
+        }
+        if (fl == 0 || fl == 1) {
+            fl = 1;
+            num1[strlen(num1)] = str[i];
+        } else {
+            num2[strlen(num2)] = str[i];
+        }
+    }
 
+    int fl1 = check_system(num1);
+    int fl2 = check_system(num2);
+    if (fl1 != fl2) {
+        return 1;
+    }
 
+    int num1 = convert(num1, fl1);
+    int num2 = convert(num2, fl2);
 
+    int result = operation(num1, num2, op);
+    print(result, fl1);
 
+    return 0;
+}
 
-
-
-
-
-
-
-
-
-
-
+int calculator(char *str) {
+    if (str[0] == '~') {
+        return for_one_number(str);
+    } else {
+        return for_two_numbers(str);
+    }
+}
