264cdda4e13e3844dbf8af23d5041664f2b6c52f
krilhoho <kirillkhokhlov641@gmail.com>
2022-12-18 19:32:21 +0300

    bin.c/.h added for binary

---
+++ bin.c
@@ -0,0 +1,30 @@
+#include "bin.h"
+#include <stdlib.h>
+#include <string.h>
+
+int fbin(char *num){
+	int res = 0;
+	for (int i = 2; i < strlen(num); i++){
+		if (num[i] >= '0' && num[i] <= '1'){
+			res = res*2 + (num[i]- '0');
+		}
+	}
+	return res;
+}
+
+char *tbin(int num){
+	char *res = (char *) malloc(sizeof(char));
+	while (num>0) {
+		int  digit = num%2;
+		res = (char *) realloc(res, (strlen(res)+1)*sizeof(char));
+		res[strlen(res)] = digit + '0';
+		num /=2;
+	}
+	char *res_rev = (char *) malloc(sizeof(char));
+	for (int i = strlen(res) - 1; i >= 0; i--){
+		res_rev = (char *) realloc(res_rev, (strlen(res_rev)+1)*sizeof(char));
+		res_rev[strlen(res_rev)] = res[i];
+	}
+	free(res);
+	return res_rev;
+}
---
+++ bin.h
@@ -0,0 +1,7 @@
+#ifndef BIN_H
+#define BIN_H
+
+int fbin(char* num);
+char *tbin(int num);
+
+#endif
